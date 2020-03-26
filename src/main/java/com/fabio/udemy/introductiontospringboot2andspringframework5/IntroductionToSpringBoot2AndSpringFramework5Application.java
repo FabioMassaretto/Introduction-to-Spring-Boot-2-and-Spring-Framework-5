19329b9a9388fe19328f3106ee7630b9099e884d
Fabio Massaretto <fmassaretto@gmail.com>
2020-03-25 23:29:44 -0300

    initial commit

---
+++ src/main/java/com/fabio/udemy/introductiontospringboot2andspringframework5/IntroductionToSpringBoot2AndSpringFramework5Application.java
@@ -0,0 +1,13 @@
+package com.fabio.udemy.introductiontospringboot2andspringframework5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntroductionToSpringBoot2AndSpringFramework5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntroductionToSpringBoot2AndSpringFramework5Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/fabio/udemy/introductiontospringboot2andspringframework5/IntroductionToSpringBoot2AndSpringFramework5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fabio.udemy.introductiontospringboot2andspringframework5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntroductionToSpringBoot2AndSpringFramework5ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
